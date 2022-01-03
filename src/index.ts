a26fa34103461a6dd88a3ee552e983338c78491c
kirkwood616 <kirkwood616@gmail.com>
2022-01-03 17:35:47 -0500

    Setting up Handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import itemRoutes from "./routes/item-routes";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -8,8 +9,17 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: false }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+
 const port = 3000;
 
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
 
 app.use("/api/cart-items", itemRoutes);
