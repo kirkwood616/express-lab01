cd68775f33333e4b793c75be9224d5f361e4444b
kirkwood616 <kirkwood616@gmail.com>
2022-01-03 18:53:47 -0500

    Adding more Handlebars

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.set("view options", { layout: "index" });
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = 3000;
@@ -20,6 +21,10 @@ app.listen(port, () => console.log(`Listening on port: ${port}.`));
 
 app.get("/", (req, res) => {
 	res.render("index");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
 });
 
 app.use("/api/cart-items", itemRoutes);
