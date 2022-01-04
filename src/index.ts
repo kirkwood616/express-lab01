ca93d4eb20258206985279ba1583d7a89ba3ed54
kirkwood616 <kirkwood616@gmail.com>
2022-01-03 19:06:54 -0500

    Building handlebars

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.set("view options", { layout: "index" });
+app.set("view options", { layout: "layouts/main" });
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = 3000;
